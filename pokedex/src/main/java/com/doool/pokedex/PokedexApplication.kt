368b2278669f7237da3dff99ee823b9a457fe413
D000L <questknight26@gmail.com>
2021-09-18 14:24:46 +0900

    init architecture

---
+++ pokedex/src/main/java/com/doool/pokedex/PokedexApplication.kt
@@ -0,0 +1,5 @@
+package com.doool.pokedex
+
+import android.app.Application
+
+class PokedexApplication : Application()
---
+++ presentation/src/main/java/com/doool/pokedex/presentation/ui/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.doool.pokedex.presentation.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.doool.pokedex.presentation.ui.theme.PokedexTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      PokedexTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+          Greeting("Android")
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String) {
+  Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+  PokedexTheme {
+    Greeting("Android")
+  }
+}
