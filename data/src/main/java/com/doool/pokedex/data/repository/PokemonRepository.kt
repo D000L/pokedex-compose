b8319632b1bae8ef4801e63d9fb07891e9915b7f
D000L <questknight26@gmail.com>
2021-09-20 02:03:55 +0900

    insert data to local when remote loaded

--- data/src/main/java/com/doool/pokedex/data/repository/PokemonRepository.kt
+++ data/src/main/java/com/doool/pokedex/data/repository/PokemonRepository.kt
@@ -1,5 +1,9 @@
 package com.doool.pokedex.data.repository
 
+import com.doool.pokedex.data.dao.PokemonDao
+import com.doool.pokedex.data.dao.PokemonDetailDao
+import com.doool.pokedex.data.entity.PokemonDetailEntity
+import com.doool.pokedex.data.entity.PokemonEntity
 import com.doool.pokedex.data.mapper.toModel
 import com.doool.pokedex.data.service.PokeApiService
 import com.doool.pokedex.domain.model.Pokemon
@@ -7,14 +11,42 @@ import com.doool.pokedex.domain.model.PokemonDetail
 import com.doool.pokedex.domain.repository.PokemonRepository
 import javax.inject.Inject
 
-class PokemonRepositoryImpl @Inject constructor(private val pokeApiService: PokeApiService) : PokemonRepository {
+class PokemonRepositoryImpl @Inject constructor(
+  private val pokeApiService: PokeApiService,
+  private val pokemonDao: PokemonDao,
+  private val pokemonDetailDao: PokemonDetailDao
+) : PokemonRepository {
 
   override suspend fun getPokemonList(offset: Int, countPerPage: Int): List<Pokemon> {
-    return pokeApiService.getPokemonList(offset, countPerPage).results.map { it.toModel() }
+    return fetchPokemonList(offset, countPerPage).map { it.toModel() }
+  }
+
+  private suspend fun fetchPokemonList(offset: Int, countPerPage: Int): List<PokemonEntity> {
+    val localResult = pokemonDao.getPokemonList(offset, offset + countPerPage)
+
+    if (localResult.isNullOrEmpty()) {
+      val remoteResult = pokeApiService.getPokemonList(offset, countPerPage).results
+      remoteResult.forEachIndexed { index, pokemon ->
+        pokemon.offset = offset + index
+      }
+      pokemonDao.insertPokemonList(remoteResult)
+      return remoteResult
+    }
+    return localResult
   }
 
   override suspend fun getPokemonDetail(name: String): PokemonDetail {
-    return pokeApiService.getPokemon(name).toModel()
+    return fetchPokemonDetail(name).toModel()
   }
 
+  private suspend fun fetchPokemonDetail(name: String): PokemonDetailEntity {
+    val localResult = pokemonDetailDao.getPokemonDetail(name)
+
+    if (localResult == null) {
+      val remoteResult = pokeApiService.getPokemon(name)
+      pokemonDetailDao.insertPokemonDetail(remoteResult)
+      return remoteResult
+    }
+    return localResult
+  }
 }
