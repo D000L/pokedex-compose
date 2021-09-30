769537ad4bb2d768956b5a544eca912fcfe505bf
D000L <questknight26@gmail.com>
2021-09-30 21:03:21 +0900

    clean code

--- domain/src/main/java/com/doool/pokedex/domain/usecase/GetPokemonPagingSource.kt
+++ domain/src/main/java/com/doool/pokedex/domain/usecase/GetPokemonPagingSource.kt
@@ -5,7 +5,7 @@ import com.doool.pokedex.domain.model.PokemonDetail
 import com.doool.pokedex.domain.repository.PokemonRepository
 import javax.inject.Inject
 
-class GetPokemonList @Inject constructor(private val pokemonRepository: PokemonRepository) {
+class GetPokemonPagingSource @Inject constructor(private val pokemonRepository: PokemonRepository) {
 
   operator fun invoke(query: String? = null): PagingSource<Int, PokemonDetail> =
     pokemonRepository.searchPokemonList(query).asPagingSourceFactory()()
