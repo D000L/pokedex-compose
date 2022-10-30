57cd3bad0b2c629af06a01666a3efcde7a4e31fd
DESKTOP-NLQA4UA\doool <questknight26@gmail.com>
2022-10-30 21:44:06 +0900

    temp

--- domain/src/main/java/com/doool/pokedex/domain/model/Pokemon.kt
+++ domain/src/main/java/com/doool/pokedex/domain/model/Pokemon.kt
@@ -1,11 +1,7 @@
 package com.doool.pokedex.domain.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class Pokemon(
     val id: Int = 0,
     val name: String = "",
     val imageUrl: String = ""
-) : Parcelable
+)
