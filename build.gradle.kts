f02c42cfcf95faad5994374fd975e6c5d22918eb
DESKTOP-NLQA4UA\doool <questknight26@gmail.com>
2022-11-11 00:21:42 +0900

    ADD: module graph & module checker library

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
         mavenCentral()
         gradlePluginPortal()
         maven("https://jitpack.io")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.1")
@@ -16,11 +16,18 @@ buildscript {
         classpath("com.google.dagger:hilt-android-gradle-plugin:$hilt_version")
         classpath("com.autonomousapps:dependency-analysis-gradle-plugin:1.2.1")
 
+        // gradle dependencyUpdates
         classpath("com.github.ben-manes:gradle-versions-plugin:$gradleVersionPlugin")
+
+        // gradlew generateProjectDependencyGraph
+        classpath("com.vanniktech:gradle-dependency-graph-generator-plugin:0.9.0-SNAPSHOT")
     }
 }
 
-apply(plugin = "com.autonomousapps.dependency-analysis")
+plugins {
+    id("com.rickbusarow.module-check") version "0.12.3"
+}
+apply(plugin="com.vanniktech.dependency.graph.generator")
 
 configurations.all {
     resolutionStrategy.cacheChangingModulesFor(0, "seconds")
