d79a0f6b15f384f719bd812d2cbf8144ba6f45b4
doool <questknight26@gmail.com>
2023-06-04 13:32:30 +0900

    Fix : Build Error

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
-    implementation("com.android.tools.build:gradle:7.3.1")
+    implementation("com.android.tools.build:gradle:8.0.2")
     implementation("com.squareup:javapoet:1.13.0")
 
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
